15594901633ce85588ba53a6d8fc73e40bd647ca
Jayden <jdecambre10@gmail.com>
2026-02-01 22:23:12 -0500

    Initial Commit

---
+++ RinkuApp/RinkuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RinkuAppApp.swift
+//  RinkuApp
+//
+//  Created by Jayden DeCambre on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct RinkuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
